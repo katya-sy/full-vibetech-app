6b293d3cd8b9058d38cbcf926731e0154b958537
katya-sy <katya_sy_28@mail.ru>
2024-01-10 13:49:46 +0900

    database connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(PORT + "slлорлорfh"));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(PORT));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
