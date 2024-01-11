76a8526435385d74de24c67ecc51eec293781c04
katya-sy <katya_sy_28@mail.ru>
2024-01-11 14:41:38 +0900

    add authorization, registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,51 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+
+const generateJWT = (id, email) => {
+  return jwt.sign({ id, email }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password } = req.body;
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользователь с таким email уже существует!")
+      );
+    }
 
-  async login(req, res) {}
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJWT(user.id, user.email);
+
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal("Пользователь с таким email не найден!"));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Неверный пароль!"));
+    }
+    const token = generateJWT(user.id, user.email);
+
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("Не задан ID!"));
-    }
-    res.json(id);
+    const token = generateJWT(req.user.id, req.user.email);
+    return res.json({ token });
   }
 }
 
